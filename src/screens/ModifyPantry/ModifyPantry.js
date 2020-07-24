cf3aa476a00edeb84dc65467c98f4cbd8d633cd4
Janavi Chadha <JanaviChadha@gmail.com>
2020-07-24 11:26:33 -0500

    Added a search bar ( no searching capabilites yet )
    This commit added the search bar to the modify pantry screen but I have not yet added actual searching capabilites yet

--- src/screens/ModifyPantry/ModifyPantry.js
+++ src/screens/ModifyPantry/ModifyPantry.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View , Searchbar } from 'react-native'
 import styles from './styles';
 import { firebase } from '../../firebase/config'
 
 export default function ModifyPantry() {
 
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const onChangeSearch = query => setSearchQuery(query);
+
+
     return (
-        <View style={styles.container}>
-            <Text>Modify Pantry Page</Text>
-        </View>
-    )
-}
+        <Searchbar ref="searchBar" placeholder="Add Ingredients" onChangeText={onChangeSearch} value={searchQuery} />
+    );
+};

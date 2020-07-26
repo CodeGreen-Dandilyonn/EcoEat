4bf5bfbba8b8eb61073729da16cc3a10855ce8cf
Anna Zhang <anna.zhang@yale.edu>
2020-07-26 16:28:14 -0700

    Added container styles

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -35,7 +35,7 @@ export default function RegistrationScreen({ navigation }) {
                     .doc(uid)
                     .set(data)
                     .then(() => {
-                        navigation.navigate('Onboarding')
+                        navigation.navigate('Page One')
                     })
                     .catch((error) => {
                         alert(error)
